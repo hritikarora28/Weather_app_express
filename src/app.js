81f454fe3ab1e21d76a476c22ea1fa7c6577e02d
Hritik Arora <hritikarora875@gmail.com>
2021-05-04 15:56:19 +0530

    Fourth Commit

--- src/app.js
+++ src/app.js
@@ -1,30 +1,43 @@
 const express = require('express');
 
 const path = require('path');
+const hbs = require('hbs')
 const app = express();
 const port = process.env.port || 8000;
 //Public static path
 const staticPath = path.join(__dirname,'../public');
-console.log(staticPath)
+const templatePath = path.join(__dirname,'../templates/views');
+const partialspath = path.join(__dirname,'../templates/partials')
+
 
 
 app.use(express.static(staticPath));
+app.set('view engine', 'hbs');
+app.set('views',templatePath);
+hbs.registerPartials(partialspath);
 
 //routing
-app.get('/', (req, res) => {
-	res.send('Welcome to home page');
-});
+
+
+
+
+
+app.get("",(req,res) => {
+	res.render('index')
+})
 
 app.get('/about', (req, res) => {
-	res.send('Welcome to about page');
+	res.render('about')
 });
 
 app.get('/weather', (req, res) => {
-	res.send('Welcome to About weather page');
+	res.render('weather');
 });
 
 app.get('*', (req, res) => {
-	res.send('Error 404 page not found');
+	res.render('404error',{
+		errormsg:"Opps Page not Found"
+	})
 });
 
 app.listen(port, () => {
