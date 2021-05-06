4f6d13cc0772580d382b152bca97a99100a00c68
Hritik Arora <hritikarora875@gmail.com>
2021-05-06 15:02:55 +0530

    final Commit

--- src/app.js
+++ src/app.js
@@ -1,33 +1,27 @@
 const express = require('express');
 
 const path = require('path');
-const hbs = require('hbs')
+const hbs = require('hbs');
 const app = express();
 const port = process.env.port || 8000;
 //Public static path
-const staticPath = path.join(__dirname,'../public');
-const templatePath = path.join(__dirname,'../templates/views');
-const partialspath = path.join(__dirname,'../templates/partials')
-
-
+const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../templates/views');
+const partialspath = path.join(__dirname, '../templates/partials');
 
 app.use(express.static(staticPath));
 app.set('view engine', 'hbs');
-app.set('views',templatePath);
+app.set('views', templatePath);
 hbs.registerPartials(partialspath);
 
 //routing
 
-
-
-
-
-app.get("",(req,res) => {
-	res.render('index')
-})
+app.get('', (req, res) => {
+	res.render('index');
+});
 
 app.get('/about', (req, res) => {
-	res.render('about')
+	res.render('about');
 });
 
 app.get('/weather', (req, res) => {
@@ -35,9 +29,9 @@ app.get('/weather', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	res.render('404error',{
-		errormsg:"Opps Page not Found"
-	})
+	res.render('404error', {
+		errormsg: 'Opps Page not Found',
+	});
 });
 
 app.listen(port, () => {
